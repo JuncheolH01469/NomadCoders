4e9ba0f66976f4f9c2816a8094c1fd47c2919f8c
JJunCH <49511543+JJunCH@users.noreply.github.com>
2020-08-12 18:51:19 +0900

    second commit

--- 바닐라 JS로 크롬 앱 만들기/#3 - Make you first js app/3_10/bg.js
+++ 바닐라 JS로 크롬 앱 만들기/#3 - Make you first js app/3_10/bg.js
@@ -4,8 +4,7 @@ const IMG_NUMBER = 30;
 
 function paintImage(imgNumber){
     const image = new Image();
-    root = `C:/Users/황준철/Desktop/개인공부_프로그래밍/NomadCoders/`;
-    image.src = `${root}바닐라 JS로 크롬 앱 만들기/image/backgrounds/Large/${imgNumber + 1}.jpg`;
+    image.src = `images/${imgNumber + 1}.jpg`;
     image.classList.add("bgImage");
     body.prepend(image);
 }
